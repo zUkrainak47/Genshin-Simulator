78f28d90b34abda28fb93ae2cc648fdbb6b5f189
zUkrainak47 <55208577+zUkrainak47@users.noreply.github.com>
2024-03-26 14:06:02 +0200

    fixed print inventory

--- simulator.py
+++ simulator.py
@@ -354,7 +354,7 @@ def print_inventory(list_of_artifacts, indexes_to_print=None):
         needed_indexes = indexes_to_print
 
     print("Inventory:\n")
-    t1 = list_of_artifacts[0].type
+    t1 = list_of_artifacts[needed_indexes[0]].type
     print('-' * 43, f'{t1}{"s" if t1 != "Sands" else ""}', '-' * 43)
 
     for this_index in range(len(needed_indexes)):
@@ -362,7 +362,7 @@ def print_inventory(list_of_artifacts, indexes_to_print=None):
 
         if this_index != 0:
             t_last = list_of_artifacts[needed_indexes[this_index - 1]].type
-            t_now = list_of_artifacts[this_index].type
+            t_now = list_of_artifacts[needed_indexes[this_index]].type
 
             if t_now != t_last:
                 print('\n' + '-' * 43, f'{t_now}{"s" if t_now != "Sands" else ""}', '-' * 43)
