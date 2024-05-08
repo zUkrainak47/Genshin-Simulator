381d7baaa63325436d93887e20b55da4e9f562b6
zUkrainak47 <55208577+zUkrainak47@users.noreply.github.com>
2024-05-08 21:21:35 +0300

    rename, tweak, beautify

---
+++ visualize_distribution.py
@@ -0,0 +1,34 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+def jsonKeys2int(x):
+    if isinstance(x, dict):
+        return {int(kk): vv for kk, vv in x.items()}
+    return x
+
+try:
+    with open('.\\banner_info\\character_distribution.txt') as file:
+        data = file.read()
+    distribution = json.loads(data, object_hook=jsonKeys2int)
+    num = distribution.pop(100)
+    s = sum(distribution.values())
+    for i in distribution:
+        distribution[i] = distribution[i]/s * 100
+
+    plt.plot(distribution.keys(), distribution.values())
+    plt.xticks([1] + list(range(4, 82, 4)) + [83, 87, 90], rotation=45)
+    plt.yticks(np.arange(0, 11, 0.6))
+    plt.ylabel('Percentage')
+    plt.xlabel('Pity')
+    plt.title(f'Distribution of 5★ characters per pity (size = {num} pulls)')
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig(f'.\\banner_info\\character distribution (size = {num}).png', dpi=500)
+    plt.show()
+
+except FileNotFoundError:
+    print('No distribution file')
+
+except:
+    print('Something went wrong')
